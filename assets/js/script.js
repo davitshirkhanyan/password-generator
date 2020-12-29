b8ed1d1fd5cde4930919075db2ecfc7bd0c45f37
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2020-12-29 15:02:21 -0800

    add variables of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // add a function to generate numbers from low to high
+let arrayFromLowToHigh = function(low, high) {
+    let passwordChars = [];
+    for(let i = low; i <= high; i++) {
+        passwordChars.push(i);
+    }
+    return passwordChars;
+}
 
+let upperCaseCharCodes = arrayFromLowToHigh(65, 90);
+let lowerCaseCharCodes = arrayFromLowToHigh(97, 122);
+let numberCharCodes = arrayFromLowToHigh(48, 57);
+let symbolCharCodes = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+).concat(
+    arrayFromLowToHigh(91, 96)
+).concat(
+    arrayFromLowToHigh(123, 126)
+);
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
