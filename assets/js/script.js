9f8a4d7a64d9b13bdbe8239b04941f7bd13c2855
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2020-12-30 16:09:11 -0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,35 @@
-// add a function to generate numbers from low to high
-let arrayFromLowToHigh = function(low, high) {
-    let passwordChars = [];
-    for(let i = low; i <= high; i++) {
-        passwordChars.push(i);
-    }
-    return passwordChars;
-}
+//Assignment Code
+let generateBtn = document.querySelector("#generate");
 
-let upperCaseCharCodes = arrayFromLowToHigh(65, 90);
-let lowerCaseCharCodes = arrayFromLowToHigh(97, 122);
-let numberCharCodes = arrayFromLowToHigh(48, 57);
-let symbolCharCodes = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58, 64)
-).concat(
-    arrayFromLowToHigh(91, 96)
-).concat(
-    arrayFromLowToHigh(123, 126)
-);
+    let lowerCaseCharCodes = 'abcdefghijklmnopqrstuvwxyz';
+    let upperCaseCharCodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let numberCharCodes = '0123456789';
+    let symbolCharCodes = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+    let confirmLowerChar = false;
+    let confirmUpperChar = false;
+    let confirmNumberChar = false;
+    let confirmSymbolChar = false;
+    let password = '';
+    let confirmLength = '';
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
+// Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
     
+
 }
 
 
+    
 
 
+// Write password to the #password input
+ function writePassword() {
+ let password = generatePassword();
+ let passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", writePassword);
 
-let generateButton=document.getElementById("generate");
-let passwordEntry=document.getElementById("password");
-generateButton.onclick = generatePassword;
